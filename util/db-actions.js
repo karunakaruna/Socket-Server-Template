0e7c6f5420b96dd78544599a72c41e2d43465066
Jonny Ostrem <jonnyawesome@gmail.com>
2023-10-26 16:18:49 -0700

    cleanup

--- util/db-actions.js
+++ util/db-actions.js
@@ -42,4 +42,14 @@ async function addDummyProfileRow() {
     }
   }
 
-  module.exports = {addDummyProfileRow};
+  async function getPostgresVersion() {
+    const client = await pool.connect();
+    try {
+      const response = await client.query('SELECT version()');
+      console.log(response.rows[0]);
+    } finally {
+      client.release();
+    }
+  }
+
+  module.exports = {addDummyProfileRow, getPostgresVersion};
--- util/dbConfig.js
+++ util/dbConfig.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
