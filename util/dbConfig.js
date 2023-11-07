11c0d7c4425f39689b0cd30c3d098c0b9b941ebf
Jonny Ostrem <jonnyawesome@gmail.com>
2023-11-07 14:25:04 -0800

    ssl

--- util/dbConfig.js
+++ util/dbConfig.js
@@ -5,8 +5,12 @@ const { Pool } = require("pg");
 const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString
-    // ssl: isProduction
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+
+    ssl: {
+        rejectUnauthorized: false // For Heroku, you may need to set this to false
+      }   
+       // ssl: isProduction
 });
 
 module.exports = { pool };
