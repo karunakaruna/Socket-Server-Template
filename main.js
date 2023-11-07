9912041d1cbe2b0ceea61b28e8ca3fd15e6f4e4c
Jonny Ostrem <jonnyawesome@gmail.com>
2023-11-07 13:55:27 -0800

    Update main.js

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ app.use('/modals', modalsRouter);
 
 //Home Index from metacarta
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile('/index.html');
 });
 
 //Homepage
