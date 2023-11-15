73ffee0cf4aa77f5b0c45199bce69950b24adaae
Jonny Ostrem <jonnyawesome@gmail.com>
2023-11-14 19:05:19 -0800

    more count debugging

--- main.js
+++ main.js
@@ -376,7 +376,7 @@ ws.on("message", (data) => {
                 users[userID].count += 1;
                 // Broadcast the updated count to all users
                 wss.clients.forEach((client) => {
-                    if (client.readyState === WebSocket.OPEN && client.userID === userID) {
+                    if (client.readyState === WebSocket.OPEN) {
                         console.log(users[userID].count, users[userID], userID);
                         client.send(JSON.stringify({ type: "count", value: users[userID].count }));
                     }
