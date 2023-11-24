3ef803c7016c56ebee78e269b801272eeb9a324a
Jonny Ostrem <jonnyawesome@gmail.com>
2023-11-23 18:09:14 -0800

    dbConfig for local

--- util/dbConfig.js
+++ util/dbConfig.js
@@ -3,14 +3,13 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString_backup = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-
-    // ssl: {
-    //     rejectUnauthorized: true // For Heroku, you may need to set this to false
-    //   }   
-       // ssl: isProduction
+    ssl: isProduction ? { rejectUnauthorized: false } : { rejectUnauthorized: false, sslmode: 'require' },
 });
+
+
 
 module.exports = { pool };
