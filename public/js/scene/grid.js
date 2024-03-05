5395c56662dfda7748847cb81d9fbf925aa3e6c3
Jonny Ostrem <jonnyawesome@gmail.com>
2024-03-05 08:34:12 -0800

    More worlds for kernel

--- public/js/scene/grid.js
+++ public/js/scene/grid.js
@@ -3,6 +3,6 @@ const gridGeometry = new THREE.PlaneGeometry(288, 288, 88, 88);
 const gridMaterial = new THREE.MeshBasicMaterial({ color: 0x333333, wireframe: true });
 const grid = new THREE.Mesh(gridGeometry, gridMaterial);
 grid.rotation.x = Math.PI / 2;
-grid.position.y = .1;
+grid.position.y = -.2;
 return grid;
 };
