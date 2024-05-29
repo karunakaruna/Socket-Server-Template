d1a54eba58d1348ff735dc56a3c4ddd12b469aad
karuna.eth <jonnyawesome@gmail.com>
2024-05-29 16:27:35 -0700

    postHOGGINit

--- public/scene.js
+++ public/scene.js
@@ -19,6 +19,7 @@ import { Lights, TextureSpot } from './js/scene/lights';
 import { Resizer } from './js/util/Resizer';
 import { submitForm, submitRegisterForm, logOut, submitEmail, updatePassword } from './submit-module.js';
 import { attachLabelToObjectsAdv } from './js/Sprite.js';
+import posthog from 'posthog-js'
 
 
 DOM();
@@ -27,7 +28,8 @@ console.log('my user ID', wsc.myUserID);
 console.log('my user object', wsc.users[wsc.myUserID]);
 const vec = new THREE.Vector3(0,0,0);
 
-
+posthog.init('phc_t82qchdUXKIr2RxxDtg1FO7ai169CvC1YLLjGt0L7DQ', { api_host: 'https://us.i.posthog.com' })   
+posthog.capture('UserID', { property: wsc.myUserID})
 // const player = new UserSphere(null, cube);
 // scene.add(player);
 // const playerlevel = player.getLevel();
